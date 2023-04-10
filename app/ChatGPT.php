ccd16ce335b5e9dc12c7f02f616874afd4051423
Joris Schelfhout <joris@shelfwood.co>
2023-04-10 17:45:59 +0200

    Add gitignore to .env

---
+++ app/ChatGPT.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Collection;
+use OpenAI;
+use OpenAI\Client;
+
+class ChatGPT
+{
+    protected Client $client;
+
+    protected Collection $messages;
+
+    public function __construct()
+    {
+        $this->client = OpenAI::client(config('openai.api_key'));
+        $this->messages = collect();
+    }
+
+    public function messages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function send(string $message): self
+    {
+        $this->messages->push([
+            'role' => 'user',
+            'content' => $message,
+        ]);
+
+        // If the message itself is too large
+        if (OpenAITokenizer::count($message) > config('openai.max_tokens')) {
+            throw new \Exception('Message is too large');
+        }
+
+        $this->trimMessagesToFit();
+
+        $message = $this->getChatResponse();
+
+        $this->messages->push($message);
+
+        return $this;
+    }
+
+    public function system(string $message): self
+    {
+        $this->messages->push([
+            'role' => 'system',
+            'content' => $message,
+        ]);
+
+        return $this;
+    }
+
+    public function reset(): self
+    {
+        $this->messages = collect();
+
+        return $this;
+    }
+
+    private function trimMessagesToFit(): void
+    {
+        $totalTokens = OpenAITokenizer::count($this->messages->pluck('content')->implode(' '));
+        echo PHP_EOL.'🔎 Total tokens: '.$totalTokens.PHP_EOL;
+        while ($totalTokens > config('openai.max_tokens')) {
+            echo '⚠️ Removing context from 1 message to abide by token limit ('.$this->messages->count().' left)'.PHP_EOL;
+            // Remove the a message from the $this->messages collection and recalculate the total tokens
+            $this->messages->shift();
+
+            $totalTokens = OpenAITokenizer::count($this->messages);
+
+            if ($totalTokens < config('openai.max_tokens')) {
+                echo PHP_EOL.'✅ Token limit is not exceeded with '.$this->messages->count().' messages left'.PHP_EOL.PHP_EOL;
+            }
+        }
+    }
+
+    private function getChatResponse(): array
+    {
+            $response = $this->client->chat()->create([
+                'model' => config('openai.model_used'),
+                'temperature' => config('openai.temperature'),
+                'messages' => $this->messages->toArray(),
+            ]);
+
+            return [
+                'role' => $response->choices[0]->message->role,
+                'content' => $response->choices[0]->message->content,
+            ];
+    }
+
+    public function receive(): string
+    {
+        echo '✉️ Response: '.$this->messages->last()['content'].PHP_EOL;
+
+        return $this->messages->last()['content'];
+    }
+}
---
+++ config/openai.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api_key' => env('OPENAI_API_KEY'),
+    'model_used' => 'gpt-3.5-turbo',
+    'max_tokens' => 4096,
+    'temperature' => 0,
+];
